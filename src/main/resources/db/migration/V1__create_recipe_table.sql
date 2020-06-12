ac2366d71853dfbbe1a2a46fe2e374690d570974
shiraga-riku <shiraga@aivick.co.jp>
2020-06-12 16:04:33 +0900

    migration

--- src/main/resources/db/migration/V1__create_recipe_table.sql
+++ src/main/resources/db/migration/V1__create_recipe_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `recipe` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
-  `kcal` double NOT NULL
+  `kcal` double NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/resources/db/migration/V2__create_menu_table.sql
+++ src/main/resources/db/migration/V2__create_menu_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `menu` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `type` varchar(10) NOT NULL,
-  `price` int NOT NULL
+  `price` int NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
